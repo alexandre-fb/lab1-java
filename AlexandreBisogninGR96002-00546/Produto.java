33c11b976c0e48ab6a7674a04cc21ff587d2bd11
alexandre-fb <alexandrefb.86@gmail.com>
2024-06-01 11:38:08 -0300

    feat: classe produto

--- AlexandreBisogninGR96002-00546/Produto.java
+++ AlexandreBisogninGR96002-00546/Produto.java
@@ -1,3 +1,34 @@
 public class Produto {
-  
+  private String nome;
+  private double preco;
+
+  public Produto(String nome, double preco) {
+    this.nome = nome;
+    this.preco = preco;
+  }
+
+  @Override
+  public String toString() {
+    return 
+      "Produto\n"
+      + "Nome: " + this.nome + "\n"
+      + "Preço: " + this.preco;
+  }
+
+  // metodos de acesso
+  public String getNome() {
+    return this.nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public double getPreco() {
+    return this.preco;
+  }
+
+  public void setPreco(double preco) {
+    this.preco = preco;
+  }
 }
