0d8ca2f5ad8ece80523e23ee28defb426dbba5b6
alexandre-fb <alexandrefb.86@gmail.com>
2024-06-14 23:41:52 -0300

    Removidos arquivos

--- AlexandreBisogninGR96002-00546/Etapa 4. Array/tempCodeRunnerFile.java
+++
@@ -1,4 +0,0 @@
-ojaInformatica.setSeguroEletronicos(800);
-		shopping.insereLoja(lojaInformatica);
-		total = shopping.quantidadeLojasPorTipo("Informática");
-		System.out.println(total == 2 ? "[OK] Método Shopping.quantidadeLojasPorTipo() - localizou duas lojas" : "[NOK] Método Shopping.qua
